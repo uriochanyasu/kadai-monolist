139cb612e58b5b85ce586341e0822584fafbb648
uriochanyasu <uriochan777@gmail.com>
2019-01-28 09:35:47 +0000

    wnat&have

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
   has_many :wants
   has_many :want_items, through: :wants, source: :item
   
-  
   def want(item)
     self.wants.find_or_create_by(item_id: item.id)
   end
@@ -22,6 +21,24 @@ class User < ApplicationRecord
   end
   
   def want?(item)
-    self.want_item.include?(item)
-  end  
+    self.want_items.include?(item)
+  end
+  
+  has_many :haves, class_name: 'Have'
+  has_many :have_items, through: :haves, source: :item
+  
+  def have(item)
+    self.haves.find_or_create_by(item_id: item.id)
+  end
+  
+  def unhave(item)
+    have = self.haves.find_by(item_id: item.id)
+    have.destroy if have
+  end
+  
+  def have?(item)
+    self.have_items.include?(item)
+  end
+
+  
 end
